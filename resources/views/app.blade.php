2b45debb35b1e47f4a886a657df2a91d6dc7da47
Carlos <odohcarlos857@gmail.com>
2026-08-18 10:59:26 +0000

    PWA

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,14 @@
     <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
     <meta http-equiv="Pragma" content="no-cache" />
     <title>MiabéStock</title>
+
+    <link rel="icon" href="/favicon.ico" sizes="any" />
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+
+    <link rel="manifest" href="/manifest.json" />
+    <meta name="theme-color" content="#1E3F73" />
+
     @viteReactRefresh
     @vite(['resources/css/app.css', 'resources/js/app.jsx'])
 </head>
